05c39ec0c73ac536aab3e7e87e50e653acc33655
Jeffrey Wilcke <jeffrey@noxa.nl>
2010-09-24 20:42:10 +0200

    Added concurrency module which includes different kind of concurrency. Concurrency module is open to implement other methods.

--- lib/generators/runner/runner_generator.rb
+++ lib/generators/runner/runner_generator.rb
@@ -3,26 +3,30 @@ require 'rails/generators/migration'
 
 class RunnerGenerator < Rails::Generators::Base
 
-  include Rails::Generators::Migration
-  
-  def self.source_root
-     @source_root ||= File.join(File.dirname(__FILE__), 'templates')
-  end
+	include Rails::Generators::Migration
+	
+	def self.source_root
+		 @source_root ||= File.join(File.dirname(__FILE__), 'templates')
+	end
 
-  # Implement the required interface for Rails::Generators::Migration.
-  #
-  def self.next_migration_number(dirname) #:nodoc:
-    next_migration_number = current_migration_number(dirname) + 1
-    if ActiveRecord::Base.timestamped_migrations
-      [Time.now.utc.strftime("%Y%m%d%H%M%S"), "%.14d" % next_migration_number].max
-    else
-      "%.3d" % next_migration_number
-    end
-  end
-  
-  def create_migration_file
-    if defined?(ActiveRecord)
-      migration_template 'migration.rb', 'db/migrate/create_runner_tasks.rb'
-    end
-  end
+	# Implement the required interface for Rails::Generators::Migration.
+	#
+	def self.next_migration_number(dirname) #:nodoc:
+		next_migration_number = current_migration_number(dirname) + 1
+		if ActiveRecord::Base.timestamped_migrations
+			[Time.now.utc.strftime("%Y%m%d%H%M%S"), "%.14d" % next_migration_number].max
+		else
+			"%.3d" % next_migration_number
+		end
+	end
+	
+	def create_initializer
+		template 'default_initializer.rb', 'config/initializers/runner.rb'
+	end
+
+	def create_migration_file
+		if defined?(ActiveRecord)
+			migration_template 'migration.rb', 'db/migrate/create_runner_tasks.rb'
+		end
+	end
 end
