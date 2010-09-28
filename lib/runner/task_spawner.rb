80227e34d58e0432af1a43364679ad09361d8e7a
Jeffrey Wilcke <jeffrey@noxa.nl>
2010-09-28 14:05:10 +0200

    Changed the initializer a little bit. Cleaner code. Added backend.before_fork

--- lib/runner/task_spawner.rb
+++ lib/runner/task_spawner.rb
@@ -10,21 +10,28 @@ module Runner
     self.max_amount_task_handlers = 5
     
     def initialize(options = {})
-      options.reverse_merge!({:amount_handlers => TaskSpawner::max_amount_task_handlers})
       @task_handlers = Array.new
       
-      options[:amount_handlers].times do 
-        @task_handlers << TaskHandler.new
+      if options[:task].blank?
+        options.reverse_merge!({:amount_handlers => TaskSpawner::max_amount_task_handlers})
+          
+        # Determine the amount of handlers required
+        amount_needed = (Task.available(TaskHandler.max_run_time).count.to_f / Runner.task_limit.to_f).ceil
+        amount_needed.times do 
+          @task_handlers << TaskHandler.new
+        end
+      else
+        @task_handlers << TaskHandler.new(:task => options[:task])
       end
       
-      self.with = options[:with] || Runner.default_spawn_method
-
-      @task_handlers.first.task = options[:task] unless options[:task].blank?
+      @with = options[:with] || Runner.default_spawn_method
     end
     
     def start_handlers
       @task_handlers.each do |task_handler|
-        concurrency(self.with) do
+        TaskHandler.backend.before_fork
+        
+        concurrency(@with) do
           # Restore connection for this fork
           TaskHandler.backend.after_fork
             
