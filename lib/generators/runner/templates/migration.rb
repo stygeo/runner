ed22c5f41eb8a60219bbac57a56dcdacc5c55c66
Jeffrey Wilcke <jeffrey@noxa.nl>
2010-11-01 11:44:55 +0100

    Changed default of finished from 0 to false.

--- lib/generators/runner/templates/migration.rb
+++ lib/generators/runner/templates/migration.rb
@@ -3,8 +3,8 @@ class CreateRunnerTasks < ActiveRecord::Migration
     create_table :background_tasks, :force => true do |table|
       table.integer   :priority, :default => 0        # Allows some jobs to jump to the front of the queue
       table.integer   :attempts, :default => 0        # Provides for retries, but still fail eventually.
-      table.boolean   :finished, :default => 0        # Displays whether the tasks is done or not
-      table.text      :run_method                       # The method in when this task should run. Imidiatly or delayed
+      table.boolean   :finished, :default => false    # Displays whether the tasks is done or not
+      table.text      :run_method                     # The method in when this task should run. Imidiatly or delayed
       table.text      :handler                        # YAML-encoded string of the object that will do work
       table.text      :last_error                     # reason for last failure (See Note below)
       table.text      :concurrency_method             # If a concurrency method is given (yield, thread or fork) save it so we can use it when the object method is invoked
